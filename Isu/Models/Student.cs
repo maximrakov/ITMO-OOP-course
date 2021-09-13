f7ddf88226e583339a11c6e83dd342bf0e81bbb2
maximrakov <78982680+maximrakov@users.noreply.github.com>
2021-09-13 22:25:10 +0300

    feat: "Implement Isu"

---
+++ Isu/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace Isu.Models
+{
+    public class Student
+    {
+        private static int _globalId = 0;
+        private string _name;
+        private Group _group;
+        private int _id;
+
+        public Student(string name, Group @group)
+        {
+            _id = _globalId++;
+            _name = name;
+            _group = @group;
+        }
+
+        public string Name => _name;
+
+        public Group Group
+        {
+            get => _group;
+            set => _group = value;
+        }
+
+        public int Id => _id;
+    }
+}
