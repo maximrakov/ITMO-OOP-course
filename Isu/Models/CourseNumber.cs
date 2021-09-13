cf7ea23825433ecf30961f61b5fb945649573e70
maximrakov <78982680+maximrakov@users.noreply.github.com>
2021-09-13 22:25:10 +0300

    add Isu

---
+++ Isu/Models/CourseNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace Isu.Models
+{
+    public class CourseNumber
+    {
+        private int _course;
+
+        public CourseNumber(int course)
+        {
+            _course = course;
+        }
+
+        public int Course => _course;
+    }
+}
---
+++ Isu/Models/Group.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Isu.Tools;
+namespace Isu.Models
+{
+    public class Group
+    {
+        private List<Student> _students = new List<Student>();
+        private int _courseNumber;
+        private string _name;
+
+        public Group(string name)
+        {
+            this._name = name;
+            this._courseNumber = Convert.ToInt32(name[2]) - 48;
+            if (name.Length != 5 || !char.IsLetter(name[0]) || !char.IsNumber(name[1]) || !char.IsNumber(name[2]) || !char.IsNumber(name[3]))
+            {
+                throw new IsuException("Invalid name of group");
+            }
+        }
+
+        public List<Student> Students => _students;
+
+        public int CourseNumber => _courseNumber;
+
+        public string Name => _name;
+
+        public Student GetStudent(int id)
+        {
+            return _students.Find(x => x.Id == id);
+        }
+
+        public Student FindStudent(string name)
+        {
+            return _students.Find(x => x.Name == name);
+        }
+
+        public void AddStudent(Student newStudent)
+        {
+            _students.Add(newStudent);
+            if (_students.Count > 27)
+            {
+                throw new IsuException("Reach max student per group");
+            }
+        }
+
+        public void DeleteStudent(Student deletableStudent)
+        {
+            _students.Remove(deletableStudent);
+        }
+    }
+}
