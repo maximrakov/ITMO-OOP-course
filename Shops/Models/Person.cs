adad99ae3549d5fa9eef06b59b2e1aa6d811413c
maximrakov <78982680+maximrakov@users.noreply.github.com>
2021-10-01 22:11:30 +0300

    fix: work with balance and transfer buyProduct method to person

--- Shops/Models/Person.cs
+++ Shops/Models/Person.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using Shops.Wrappers;
-
 namespace Shops.Models
 {
     public class Person
@@ -8,11 +7,21 @@ namespace Shops.Models
         public Person(int balance, string name)
         {
             Products = new List<PersonProductInfo>();
-            Balance = balance;
+            Balance = new Balance(balance);
             Name = name;
         }
 
-        public int Balance { get; set; }
+        public void BuyProducts(Shop shop, List<PersonProductInfo> personProduct)
+        {
+            int cost = shop.MakePurchase(personProduct);
+            Balance.Spend(cost);
+            foreach (PersonProductInfo productInfo in personProduct)
+            {
+                Products.Add(productInfo);
+            }
+        }
+
+        public Balance Balance { get; set; }
         public string Name { get; set; }
         public List<PersonProductInfo> Products { get; set; }
     }
