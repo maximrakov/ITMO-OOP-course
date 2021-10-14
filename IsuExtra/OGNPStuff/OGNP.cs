220b0165dfbb4eeca891931f6a65297e5fbba0c3
maximrakov <78982680+maximrakov@users.noreply.github.com>
2021-10-14 11:28:06 +0300

    feat: implement IsuExtra

---
+++ IsuExtra/OGNPStuff/OGNP.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Isu.Models;
+namespace IsuExtra.OGNPStuff
+{
+    public class OGNP
+    {
+        public string OGNPName { get; set; }
+        private List<Flow> flows;
+
+        public OGNP(string name)
+        {
+            OGNPName = name;
+            flows = new List<Flow>();
+        }
+
+        public Flow RegisterFlow(string groupName, int freePlaces)
+        {
+            if (groupName is null)
+            {
+                throw new ArgumentNullException(nameof(groupName));
+            }
+
+            var flow = new Flow(OGNPName + groupName, freePlaces);
+            flows.Add(flow);
+            return flow;
+        }
+
+        public void RegisterStudentForFlow(Student student, Flow flow)
+        {
+            flow.AddStudent(student);
+        }
+
+        public void AddClassesToFlow(Flow flow, string time, string teacher, string auditory)
+        {
+            if (flow is null)
+            {
+                throw new ArgumentNullException(nameof(flow));
+            }
+
+            if (teacher is null)
+            {
+                throw new ArgumentNullException(nameof(teacher));
+            }
+
+            if (auditory is null)
+            {
+                throw new ArgumentNullException(nameof(auditory));
+            }
+
+            flow.AddClasses(time, teacher, auditory);
+        }
+
+        public Flow FindStudentsFlow(Student student)
+        {
+            foreach (Flow flow in flows)
+            {
+                if (flow.HasStudent(student))
+                {
+                    return flow;
+                }
+            }
+
+            return null;
+        }
+
+        public void CancelRegistration(Student student)
+        {
+            Flow flow = FindStudentsFlow(student);
+            flow.DeleteStudent(student);
+        }
+
+        public List<Flow> GetFlows()
+        {
+            return flows;
+        }
+    }
+}
---
+++ IsuExtra/Tools/OGNPException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IsuExtra.Tools
+{
+    public class OGNPException : Exception
+    {
+        public OGNPException()
+        {
+        }
+
+        public OGNPException(string message)
+            : base(message)
+        {
+        }
+
+        public OGNPException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
