8e43f3ed535d5e58c0c24d0a5c0d0d8b835263a8
maximrakov <78982680+maximrakov@users.noreply.github.com>
2021-10-15 19:57:13 +0300

    refactor: change class name and remove usless part of code

--- Isu/Program.cs
+++ Isu/Program.cs
@@ -1,14 +1,9 @@
-﻿using System;
-
-namespace Isu
+﻿namespace Isu
 {
     internal class Program
     {
         private static void Main()
         {
-            var dt = new DateTime(2000, 1, 1, 22, 10, 0);
-            var dt2 = new DateTime(2000, 1, 1, 22, 15, 0);
-            Console.WriteLine(dt2.Subtract(dt));
         }
     }
 }
